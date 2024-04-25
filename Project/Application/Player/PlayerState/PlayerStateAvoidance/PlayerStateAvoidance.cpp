c6efd9f712f96927f03240f50b896919997f0e16
野村優羽 <@MSI>
2024-04-25 13:45:21 +0900

    移動距離の修正

--- Project/Application/Player/PlayerState/PlayerStateAvoidance/PlayerStateAvoidance.cpp
+++ Project/Application/Player/PlayerState/PlayerStateAvoidance/PlayerStateAvoidance.cpp
@@ -21,23 +21,27 @@ void PlayerStateAvoidance::Initialize()
 void PlayerStateAvoidance::Update()
 {
 
-	Vector3 velocity = { 0.0f, 0.0f, 0.0f };
+	if (frameCount_ > moveStartFrame_) {
 
-	WorldTransform* worldTransform = player_->GetWorldTransformAdress();
+		Vector3 velocity = { 0.0f, 0.0f, 0.0f };
 
-	Vector3 move = { 0.0f,0.0f,1.0f };
+		WorldTransform* worldTransform = player_->GetWorldTransformAdress();
 
-	// 移動量に速さを反映
-	move = Vector3::Multiply(spped_, Vector3::Normalize(move));
+		Vector3 move = { 0.0f,0.0f,1.0f };
 
-	// 移動ベクトルをカメラの角度だけ回転する
-	move = Matrix4x4::TransformNormal(move, worldTransform->rotateMatrix_);
+		// 移動量に速さを反映
+		move = Vector3::Multiply(spped_, Vector3::Normalize(move));
 
-	// 移動
-	velocity.x = move.x;
-	velocity.z = move.z;
+		// 移動ベクトルをカメラの角度だけ回転する
+		move = Matrix4x4::TransformNormal(move, worldTransform->rotateMatrix_);
 
-	worldTransform->transform_.translate = Vector3::Add(worldTransform->transform_.translate, velocity);
+		// 移動
+		velocity.x = move.x;
+		velocity.z = move.z;
+
+		worldTransform->transform_.translate = Vector3::Add(worldTransform->transform_.translate, velocity);
+
+	}
 
 	if (++frameCount_ >= frame_) {
 		playerStateNo_ = PlayerState::kPlayerStateRoot;
