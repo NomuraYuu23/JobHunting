5081a128b33dd28b0e96f255c92ba6dcef60528b
野村優羽 <@MSI>
2024-04-25 22:47:50 +0900

    クリアシーン(仮)

--- Project/Application/Scene/ClearScene/ClearScene.h
+++ Project/Application/Scene/ClearScene/ClearScene.h
@@ -58,9 +58,22 @@ private: // メンバ変数
 
 	bool isDecreasingVolume = true;
 
-	// スカイドーム
-	std::unique_ptr<Skydome> skydome_;
-	std::unique_ptr<Model> skydomeModel_;
+	// クリア
+	std::unique_ptr<Sprite> clearSprite_;
+	uint32_t clearTextureHandle_;
+
+	// ボタン
+	std::unique_ptr<Sprite> buttonSprite_;
+	uint32_t buttonTextureHandle_;
+	// 点滅用媒介変数
+	float buttonAlphaT_;
+	// 点滅用媒介変数速度
+	float buttonAlphaTSpeed_;
+	// 点滅用媒介変数は増えるか
+	bool buttonItIncreaseAlphaT_;
+	// 色
+	Vector4 buttonColor_;
+
 
 };
 
