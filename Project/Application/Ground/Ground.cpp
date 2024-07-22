8369fc2d375dfcc8cc535261e64ff970ed344ba7
野村優羽 <@MSI>
2024-07-22 16:06:51 +0900

    地面のテクスチャ変更

--- Project/Application/Ground/Ground.cpp
+++ Project/Application/Ground/Ground.cpp
@@ -10,6 +10,14 @@ void Ground::Initialize(LevelData::MeshData* data)
 	*colliderShape = obb;
 	collider_.reset(colliderShape);
 
+	material_->SetEnableLighting(BlinnPhongReflection);
+
+	EulerTransform uvTransform;
+	uvTransform.scale = { 100.0f, 100.0f,1.0f };
+	uvTransform.rotate = { 0.0f,0.0f,0.0f };
+	uvTransform.translate = { 0.0f,0.0f,0.0f };
+	material_->SetUvTransform(uvTransform);
+
 }
 
 void Ground::Update()
