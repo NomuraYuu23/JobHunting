a1d55781fe6ae85c728472c79fb7326f9e698ea5
野村優羽 <@MSI>
2024-09-23 17:25:47 +0900

    積分フェーズCS

--- Project/Engine/Physics/Cloth/ClothGPU.cpp
+++ Project/Engine/Physics/Cloth/ClothGPU.cpp
@@ -515,6 +515,88 @@ void ClothGPU::PipelineStateCSInitializeForUpdateExternalOperation(ID3D12Device*
 
 void ClothGPU::PipelineStateCSInitializeForUpdateIntegral(ID3D12Device* device)
 {
+
+	D3D12_ROOT_SIGNATURE_DESC descriptionRootsignature{};
+	descriptionRootsignature.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+
+	// ルートパラメータ
+	D3D12_ROOT_PARAMETER rootParameters[4] = {};
+
+	// UAV * 1
+	D3D12_DESCRIPTOR_RANGE descriptorRange[1] = {};
+	descriptorRange[0].BaseShaderRegister = 0;//iから始まる
+	descriptorRange[0].NumDescriptors = 1;//数は一つ
+	descriptorRange[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_UAV;//UAVを使う
+	descriptorRange[0].OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;//Offsetを自動計算
+
+	rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;   //CBVを使う
+	rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL; //全てで使う
+	rootParameters[0].Descriptor.ShaderRegister = 0;//レジスタ番号indexとバインド
+
+	rootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;   //CBVを使う
+	rootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL; //全てで使う
+	rootParameters[1].Descriptor.ShaderRegister = 1;//レジスタ番号indexとバインド
+
+	rootParameters[2].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;   //CBVを使う
+	rootParameters[2].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL; //全てで使う
+	rootParameters[2].Descriptor.ShaderRegister = 2;//レジスタ番号indexとバインド
+
+	rootParameters[3].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;//DescriptorTableを使う
+	rootParameters[3].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;//全てで使う
+	rootParameters[3].DescriptorTable.pDescriptorRanges = descriptorRange;//Tableの中身の配列を指定
+	rootParameters[3].DescriptorTable.NumDescriptorRanges = _countof(descriptorRange);//Tableで利用する数
+
+	descriptionRootsignature.pParameters = rootParameters; //ルートパラメータ配列へのポインタ
+	descriptionRootsignature.NumParameters = _countof(rootParameters); //配列の長さ
+
+	// サンプラー
+	D3D12_STATIC_SAMPLER_DESC samplerDesc[1] = {};
+	samplerDesc[0].Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
+	samplerDesc[0].AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	samplerDesc[0].AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	samplerDesc[0].AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	samplerDesc[0].MipLODBias = 0.0f;
+	samplerDesc[0].MaxAnisotropy = 0;
+	samplerDesc[0].ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER;
+	samplerDesc[0].BorderColor = D3D12_STATIC_BORDER_COLOR_OPAQUE_BLACK;
+	samplerDesc[0].MinLOD = 0.0f;
+	samplerDesc[0].MaxLOD = 3.402823466e+38f;
+	samplerDesc[0].RegisterSpace = 0;
+	samplerDesc[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+	descriptionRootsignature.pStaticSamplers = samplerDesc;
+	descriptionRootsignature.NumStaticSamplers = _countof(samplerDesc);
+
+	//シリアライズしてバイナリにする
+	ID3DBlob* signatureBlob = nullptr;
+	ID3DBlob* errorBlob = nullptr;
+	HRESULT hr = D3D12SerializeRootSignature(&descriptionRootsignature,
+		D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlob);
+	if (FAILED(hr)) {
+		Log::Message(reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
+		assert(false);
+	}
+
+	hr = device->CreateRootSignature(0, signatureBlob->GetBufferPointer(),
+		signatureBlob->GetBufferSize(), IID_PPV_ARGS(&rootSignaturesCS_[kPipelineStateCSIndexUpdateIntegral]));
+	assert(SUCCEEDED(hr));
+
+	// シェーダコンパイル
+	IDxcBlob* shader = CompileShader::Compile(
+		L"Resources/shaders/ClothGPU/ClothUpdateIntegral.CS.hlsl",
+		L"cs_6_0",
+		L"main");
+
+	// パイプライン
+	D3D12_COMPUTE_PIPELINE_STATE_DESC desc{};
+	desc.CS.pShaderBytecode = shader->GetBufferPointer();
+	desc.CS.BytecodeLength = shader->GetBufferSize();
+	desc.Flags = D3D12_PIPELINE_STATE_FLAG_NONE;
+	desc.NodeMask = 0;
+	desc.pRootSignature = rootSignaturesCS_[kPipelineStateCSIndexUpdateIntegral].Get();
+
+	hr = device->CreateComputePipelineState(&desc, IID_PPV_ARGS(&pipelineStatesCS_[kPipelineStateCSIndexUpdateIntegral]));
+	assert(SUCCEEDED(hr));
+
 }
 
 void ClothGPU::PipelineStateCSInitializeForUpdateSpring(ID3D12Device* device)
@@ -1185,6 +1267,10 @@ void ClothGPU::UpdateCS(ID3D12GraphicsCommandList* commandList)
 
 	UAVBarrier(commandList);
 
+	UpdateIntegralCS(commandList);
+
+	UAVBarrier(commandList);
+
 	UpdateVertexCS(commandList);
 
 	UAVBarrier(commandList);
@@ -1213,6 +1299,23 @@ void ClothGPU::UpdateExternalOperationCS(ID3D12GraphicsCommandList* commandList)
 
 void ClothGPU::UpdateIntegralCS(ID3D12GraphicsCommandList* commandList)
 {
+
+	// SRV
+	ID3D12DescriptorHeap* ppHeaps[] = { SRVDescriptorHerpManager::descriptorHeap_.Get() };
+	commandList->SetDescriptorHeaps(_countof(ppHeaps), ppHeaps);
+
+	commandList->SetPipelineState(pipelineStatesCS_[kPipelineStateCSIndexUpdateIntegral].Get());//PS0を設定
+	commandList->SetComputeRootSignature(rootSignaturesCS_[kPipelineStateCSIndexUpdateIntegral].Get());
+
+	commandList->SetComputeRootConstantBufferView(0, NumsBuff_->GetGPUVirtualAddress());
+
+	commandList->SetComputeRootConstantBufferView(1, perFrameBuff_->GetGPUVirtualAddress());
+
+	commandList->SetComputeRootConstantBufferView(2, clothCalcDataBuff_->GetGPUVirtualAddress());
+
+	commandList->SetComputeRootDescriptorTable(3, massPointUavHandleGPU_);
+
+	commandList->Dispatch((NumsMap_->massPointNum_ + 1023) / 1024, 1, 1);
 
 }
 
--- Project/Resources/shaders/ClothGPU/ClothUpdateIntegral.CS.hlsl
+++ Project/Resources/shaders/ClothGPU/ClothUpdateIntegral.CS.hlsl
@@ -1,4 +1,49 @@
-[numthreads(1, 1, 1)]
-void main( uint3 DTid : SV_DispatchThreadID )
+#include "ClothGPU.CS.hlsli"
+#include "../RandomGenerator/RandomGenerator.hlsli"
+
+ConstantBuffer<Nums> gNums : register(b0);
+
+ConstantBuffer<PerFrame> gPerFrame : register(b1);
+
+ConstantBuffer<ClothCalcData> gClothCalcData : register(b2);
+
+RWStructuredBuffer<ClothMassPoint> gClothMassPoints : register(u0);
+
+[numthreads(1024, 1, 1)]
+void main(uint32_t3 dispatchId : SV_DispatchThreadID)
 {
+
+	uint32_t index = dispatchId.x;
+
+	if (gNums.massPointNum_ > index) {
+
+		ClothMassPoint massPoint = gClothMassPoints[index];
+		// 重力
+		float32_t3 force = float32_t3( 0.0f, -9.8f, 0.0f );
+		// 風力
+		float32_t3 wind = (float32_t3)0;
+		force += wind;
+		// 変位に変換
+		force = force * (gPerFrame.deltaTime * gPerFrame.deltaTime * 0.5f * rcp(gClothCalcData.mass_));
+
+		// 抵抗
+		float resistance = 1.0f - gClothCalcData.speedResistance_ * gPerFrame.deltaTime;
+
+		// 変位
+		float32_t3 dx = (float32_t3)0;
+		// 速度
+		dx = massPoint.position_ - massPoint.prePosition_;
+		// 前フレーム位置更新
+		gClothMassPoints[index].prePosition_ = massPoint.position_;
+		// 力の変位を足しこむ
+		dx = dx + force + wind;
+		// 抵抗
+		dx *= resistance;
+
+		// 位置更新
+		dx *= massPoint.weight_; // 固定されてるか
+		gClothMassPoints[index].position_ += dx;
+	
+	}
+
 }
